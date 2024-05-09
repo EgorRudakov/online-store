039d9670dfd2fadf099fd1ab13a96ec9fc5dea07
Egor Rudakov <egor.rudakov90@gmail.com>
2024-05-09 12:56:54 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '../scss/_style.scss'
+// Импортируйте Bootstrap CSS
+import '~bootstrap/scss/bootstrap.scss'
+
+// Импортируйте Bootstrap JavaScript
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			'~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+		},
+	},
+})
