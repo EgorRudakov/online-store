4c64c090f1173babac27d6a11fc960d999158bd9
Egor Rudakov <egor.rudakov90@gmail.com>
2024-05-28 22:05:26 +0300

    chore: update sass to 1.77.2 and add prettier 3.2.5

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			'~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
-		},
-	},
-	css: {
-		minify: false,
-		sourcemap: true,
-	},
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  css: {
+    minify: false,
+    sourcemap: true,
+  },
 })
